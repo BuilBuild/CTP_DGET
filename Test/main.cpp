a1b2828518e882e6ab7579985d0112c77e324517
BuiBuild <lei15557570906@outlook.com>
2024-12-22 16:12:13 +0800

    init

---
+++ Test/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * @Author: LeiJiulong
+ * @Date: 2024-12-22 16:00:22
+ * @LastEditors: LeiJiulong && lei15557570906@outlook.com
+ * @LastEditTime: 2024-12-22 16:00:23
+ * @Description: 
+ */
+#include<iostream>
+
+int main()
+{
+    std::cout << "test" << std::endl;
+    return 0;
+}
---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * @Author: LeiJiulong
+ * @Date: 2024-12-22 16:04:34
+ * @LastEditors: LeiJiulong && lei15557570906@outlook.com
+ * @LastEditTime: 2024-12-22 16:05:37
+ * @Description: 
+ */
+
+#include <iostream>
+
+int main()
+{
+
+    std::cout << "src" << std::endl;
+    return 0;
+}
+
